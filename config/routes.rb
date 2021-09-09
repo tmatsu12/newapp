d0eff33d402210720170566c517fe05f36895359
tmatsu12 <tmatsunaga65@gmail.com>
2021-09-09 02:53:00 +0000

    [Finish]guestlogin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'homes#top'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+  devise_for :users
 end

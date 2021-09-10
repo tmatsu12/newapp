0e7090be3a20be8616297e1eb442e9154d0aa099
tmatsu12 <tmatsunaga65@gmail.com>
2021-09-10 04:06:06 +0000

    [Update]newapp

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
   devise_for :users
+  get '/shikoku' => 'homes#shikoku', as: 'shikoku'
 end

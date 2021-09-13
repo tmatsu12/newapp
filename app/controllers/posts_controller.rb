fb8d4e94fcbfb98d165fd0bfcd6c07838cee6dce
tmatsu12 <tmatsunaga65@gmail.com>
2021-09-13 08:15:34 +0000

    [Update]newapp

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,5 +9,20 @@ class PostsController < ApplicationController
 
   def new
     @prefecture = Prefecture.find(params[:prefecture_id])
+    @post = Post.new
+  end
+
+  # def create
+  #   @post = Post.new(post_params)
+  #   @post.prefecture_id = params[:prefecture_id]
+  #   @post.user_id = 1
+  #   @post.save
+  #   redirect_to posts_path(prefecture_id: params[:prefecture_id])
+  # end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :prefecture_id, :user_id)
   end
 end

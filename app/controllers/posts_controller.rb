19db15ddd50c309a257809a299902711eee24405
tmatsu12 <tmatsunaga65@gmail.com>
2021-09-13 03:00:30 +0000

    [Update]newapp

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
 
   def index
     @prefecture = Prefecture.find(params[:prefecture_id])
-    @posts = @prefecture.posts
+    @posts = @prefecture.posts.all.order(updated_at: :desc)
     @residents = @prefecture.residents
     @wannalivings = @prefecture.wannalivings
   end

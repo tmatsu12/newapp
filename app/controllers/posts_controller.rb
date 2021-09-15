6c666625e19e9afd1d48c401b8cc4474b2d3f726
tmatsu12 <tmatsunaga65@gmail.com>
2021-09-15 00:53:34 +0000

    [Update]newapp

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,6 +22,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :body, :prefecture_id, :image)
+    params.require(:post).permit(:title, :body, :prefecture_id, :image, :evaluation)
   end
 end

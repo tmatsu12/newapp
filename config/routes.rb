db570c6a7aac03b76a47d809258e3584c20aead6
tmatsu12 <tmatsunaga65@gmail.com>
2021-09-18 09:32:24 +0000

    [Update]newapp

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
+  get 'users/update'
   root 'homes#top'
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'

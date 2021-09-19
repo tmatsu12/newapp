755f01ffeb859a6cf8e319116c338870fad527e2
tmatsu12 <tmatsunaga65@gmail.com>
2021-09-19 05:32:32 +0000

    [Update]newapp

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,14 @@ class Post < ApplicationRecord
   belongs_to :user
   attachment :image
   geocoded_by :address
-  
-  
+
+  validates :title, presence: true
+  validates :city, presence: true
+  validates :body, presence: true
+  validates :body1, presence: true
+  validates :body2, presence: true
+  validates :body3, presence: true
+
+  validates :title, length: { maximum: 25 }
+  validates :city, length: { maximum: 20 }
 end

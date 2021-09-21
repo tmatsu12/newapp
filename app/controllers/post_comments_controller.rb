b04889a627b26c405fa8e4745d9d069c8949e52d
tmatsu12 <tmatsunaga65@gmail.com>
2021-09-21 02:03:25 +0000

    [Update]newapp

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,7 +1,7 @@
 class PostCommentsController < ApplicationController
   def new
     @post = Post.find(params[:post_id])
-    @post_comment = @post.comments.new(parent_id: params[:parent_id])
+    @post_comment = @post.post_comments.new(parent_id: params[:parent_id])
   end
 
   def create

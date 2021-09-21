9fa5d2a59737f1de78728f9dc6acbb37a104247c
tmatsu12 <tmatsunaga65@gmail.com>
2021-09-21 01:51:06 +0000

    [Update]newapp

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,4 +1,9 @@
 class PostCommentsController < ApplicationController
+  def new
+    @post = Post.find(params[:post_id])
+    @post_comment = @post.comments.new(parent_id: params[:parent_id])
+  end
+
   def create
     @post = Post.find(params[:post_id])
     @post_comment = PostComment.new(post_comment_params)
--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -1,6 +1,8 @@
 class PostComment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+  belongs_to :parent, class_name: 'PostComment', optional: true
+  has_many   :replies, class_name: 'PostComment', foreign_key: :parent_id, dependent: :destroy
 
   validates :comment, presence: true
   validates :comment, length: { maximum: 1000 }

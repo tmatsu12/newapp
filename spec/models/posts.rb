aa50320eaff8a48b7f8a74fd8f0c94b058ca0645
tmatsu12 <tmatsunaga65@gmail.com>
2021-09-23 23:16:18 +0000

    [Update]newapp

--- spec/models/posts.rb
+++ spec/models/posts.rb
@@ -4,7 +4,10 @@ FactoryBot.define do
     body { Faker::Lorem.characters(number: 20) }
     body1 { Faker::Lorem.characters(number: 20) }
     body2 { Faker::Lorem.characters(number: 20) }
-    body3 { Faker:Lorem.characters(number: 20) }
+    body3 { Faker::Lorem.characters(number: 20) }
+    prefecture_id { Faker::Number.within(range: 35..38) }
+    city { Faker::Address.city }
+    evaluation { Faker::Number.within(range: 1..5) }
     association :user
     association :prefecture
   end

fffed5621896cc69a7a2a247a621263c686997f5
tmatsu12 <tmatsunaga65@gmail.com>
2021-09-25 05:40:04 +0000

    [Update]newapp

--- spec/system/spec.rb
+++ spec/system/spec.rb
@@ -1,15 +1,33 @@
 require 'rails_helper'
 
-describe 'test0' do
-  describe 'top_view' do
+describe 'ユーザーログイン前のテスト' do
+  describe 'トップ画面のテスト' do
     before do
       visit root_path
     end
-    
-    context 'confirmation' do
-      it 'correct URL' do
+
+    context '表示内容の確認' do
+      it 'URLが正しい' do
         expect(current_path).to eq '/'
       end
     end
   end
+end
+
+describe 'ユーザー新規登録のテスト' do
+  let(:user) { create(:user) }
+  
+  before do
+    visit new_user_registration_path
+    fill_in 'user[name]', with: user.name
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: user.password
+    fill_in 'user[password_confirmation]', with: user.password_confirmation
+  end
+  
+  context '新規登録成功のテスト' do
+    it '正しく新規登録される' do
+    end
+  end
+  
 end

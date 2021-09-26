d53ed437f80d8f6dd77d1645f0382ff8b1bb19dc
tmatsu12 <tmatsunaga65@gmail.com>
2021-09-26 06:33:08 +0000

    [Update]newapp

--- spec/system/test_spec.rb
+++ spec/system/test_spec.rb
@@ -14,7 +14,7 @@ describe 'ユーザーログイン前のテスト' do
   end
 end
 
-describe 'ユーザー新規登録のテスト' do
+describe 'エラー①：ユーザー新規登録のテスト' do
   let(:user) { build(:user) }
 
   before do
@@ -26,31 +26,36 @@ describe 'ユーザー新規登録のテスト' do
   end
 
   context '新規登録成功のテスト' do
-    it '正しく新規登録される' do
+    it '外部キー入力を求められることなく正しく新規登録される' do
       expect { click_button "新規登録" }.to change{ User.count }.by(1)
     end
   end
 
 end
 
-describe '新規投稿時の画面遷移のテスト' do
+describe 'エラー②：新規登録時や簡単ログイン時の画面遷移のテスト' do
   let(:user) { build(:user) }
-  let!(:prefecture) { create(:prefecture) } #!がないとindex pageのidとnew pageのURLのidが一致しない？
+  let!(:prefecture) { create(:prefecture) } #!がないとindex pageのidとnew pageのURLのidが一致しない？(prefecture.id = within(35..38)とした時) → そんなことはなかった
 
   before do
-    # visit posts_path(prefecture_id: prefecture.id)
-    visit '/posts?prefecture_id=#{prefecture.id}'
-    binding.pry
-    click_button "新規に投稿する"
-    fill_in 'user[name]', with: user.name
-    fill_in 'user[email]', with: user.email
-    fill_in 'user[password]', with: user.password
-    fill_in 'user[password_confirmation]', with: user.password_confirmation
-    click_button "新規登録"
+    visit '/posts?prefecture_id=' + prefecture.id.to_s
+    click_link "新規に投稿する"
   end
 
-  it '新規登録後に新規投稿画面に遷移する' do
-    expect(current_path).to eq '/posts/' + 'new?prefecture_id=' + prefecture.id.to_s
+  context '「新規に投稿する」を押下後に新規登録した場合' do
+
+    before do
+      fill_in 'user[name]', with: user.name
+      fill_in 'user[email]', with: user.email
+      fill_in 'user[password]', with: user.password
+      fill_in 'user[password_confirmation]', with: user.password_confirmation
+      click_button "新規登録"
+    end
+
+    it '新規登録後も新規投稿画面に留まる' do
+      expect(current_path).to eq '/posts/new?prefecture_id=' + prefecture.id.to_s
+      # expect(current_path).to eq "/"
+    end
   end
 
 end

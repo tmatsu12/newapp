25cb15bbba563d6a3edb30210c22bcdd54745320
tmatsu12 <tmatsunaga65@gmail.com>
2021-09-27 01:58:48 +0000

    [Update]newapp

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -42,7 +42,10 @@ class PostsController < ApplicationController
       redirect_to post_path(@post)
     else
       @user = current_user
-      render :new
+      flash[:notice] = @post.errors.full_messages
+      @post = Post.new(post_params)
+      redirect_to new_post_path(prefecture_id: @prefecture.id)
+      # render :new
     end
   end
 

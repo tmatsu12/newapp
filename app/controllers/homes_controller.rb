3d6e6211f7d6effc8b3ac43d3349f6f40091c048
tmatsu12 <tmatsunaga65@gmail.com>
2021-10-05 23:05:06 +0000

    [Update]newapp

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,6 +2,9 @@ class HomesController < ApplicationController
   def top
   end
 
+  def about
+  end
+
   def region
     # render :partial => "homes/#{params[:region]}"
     render "region/#{params[:region]}"
